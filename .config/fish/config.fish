76de279b3fe5b6bc5b32b018519171dc4a8b7259
Justin Or <me@justinor.dev>
2023-09-28 22:39:45 +1000

    astronvim and fish update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -x DISPLAY "$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0"
+    # set -x DISPLAY "$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0"
 end
+
